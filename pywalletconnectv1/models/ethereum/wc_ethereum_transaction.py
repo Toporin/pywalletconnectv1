d68bfd8ad31e850c231beea33628be481cc85d39
Toporin <satochip.wallet@gmail.com>
2022-05-18 09:05:54 +0100

    Add support for EIP1559 in WCEthereumTransaction()

--- pywalletconnectv1/models/ethereum/wc_ethereum_transaction.py
+++ pywalletconnectv1/models/ethereum/wc_ethereum_transaction.py
@@ -1,4 +1,4 @@
-from pykson import JsonObject, StringField
+from pykson import JsonObject, StringField, IntegerField, ListField
 
 class WCEthereumTransaction(JsonObject):
     from_= StringField(serialized_name="from")
@@ -9,3 +9,8 @@ class WCEthereumTransaction(JsonObject):
     gasLimit= StringField(null= True)
     value= StringField(null= True)
     data= StringField()
+    type_= IntegerField(serialized_name="type", null= True)  # EIP-2718
+    maxPriorityFeePerGas = StringField(null= True) # EIP-1559
+    maxFeePerGas= StringField(null= True) # EIP-1559
+    accessList= ListField(str, null=True) # EIP-2930 #TODO!
+    chainId= IntegerField(null= True) 
